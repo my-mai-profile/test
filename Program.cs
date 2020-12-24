7ed4b475edf965071e7ce7c93a8d3085f4ad0d49
Petr <dartax007@gmail.com>
2020-12-24 21:36:52 +0300

    Первый проект

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Я думал руки до ООП не дойдут\n");
+        }
+    }
+}
